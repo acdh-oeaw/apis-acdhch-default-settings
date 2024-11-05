5fe6f1072ceb3574090bcaef07271c0fd22cc14a
KK <dev-kk@oeaw.ac.at>
2024-11-05 11:47:00 +0100

    feat(utils): request imprint based on settings
    Run request for imprint URL in get_imprint function
    only when both base_url and redmine_id are set.
    Prevents redundant unsuccessful requests which
    produce unhelpful log messages by urllib3 when e.g.
    a project has not yet been deployed (i.e. does not
    have a REDMINE_ID yet) or when ACDH_IMPRINT_URL
    is not a recognisable web address.
    
    Solves: #118

--- apis_acdhch_default_settings/utils.py
+++ apis_acdhch_default_settings/utils.py
@@ -3,14 +3,17 @@ import requests
 import os
 
 from django.conf import settings
+from urllib.parse import urlparse
 
 
 @functools.cache
 def get_imprint() -> str:
     base_url = getattr(settings, "ACDH_IMPRINT_URL", "https://imprint.acdh.oeaw.ac.at/")
     redmine_id = getattr(settings, "REDMINE_ID", os.getenv("SERVICE_ID", ""))
+    r = None
 
-    r = requests.get(f"{base_url}{redmine_id}")
+    if urlparse(base_url).scheme and urlparse(base_url).netloc and redmine_id:
+        r = requests.get(f"{base_url}{redmine_id}")
 
     if r and redmine_id:
         return r.text
