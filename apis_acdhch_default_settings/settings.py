1208d9c66c6145c0d463217f13b59592059ec6f9
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-12-11 13:10:12 +0100

    fix(settings): set DEFAULT_AUTO_FIELD

--- apis_acdhch_default_settings/settings.py
+++ apis_acdhch_default_settings/settings.py
@@ -165,6 +165,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/stable/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Internationalization
 # https://docs.djangoproject.com/en/stable/topics/i18n/
