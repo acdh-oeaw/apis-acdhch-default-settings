fa62941d79fd20672966d197a744a2706a708789
Birger Schacht <birger.schacht@oeaw.ac.at>
2024-12-11 12:52:57 +0100

    feat(deps): add django-removals dependency and add it to installed apps
    Closes: #122

--- apis_acdhch_default_settings/settings.py
+++ apis_acdhch_default_settings/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_removals",
     "crispy_forms",
     "crispy_bootstrap4",
     "django_filters",
