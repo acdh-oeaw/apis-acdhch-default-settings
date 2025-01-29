7d1ee88caf9f51199eec9464c2e31b32ac33f9d2
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-01-29 08:21:30 +0100

    fix(settings): drop deprecated USE_L10N setting
    Closes: #144

--- apis_acdhch_default_settings/settings.py
+++ apis_acdhch_default_settings/settings.py
@@ -164,7 +164,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LANGUAGE_CODE = "en"
 TIME_ZONE = "UTC"
 USE_I18N = True
-USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
