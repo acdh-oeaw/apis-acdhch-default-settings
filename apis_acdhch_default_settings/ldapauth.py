0f69b3b4c38e587446ab88d343ef880a79cf47b6
Birger Schacht <birger.schacht@oeaw.ac.at>
2025-10-01 09:12:30 +0200

    feat(ldapauth): allow to define the user list in a file
    If the AUTH_LDAP_USER_LIST points to an existing file, the
    CustomLDAPBackend uses it as the source for the account names that
    should be allowed to login using ldap.
    
    Closes: #252

--- apis_acdhch_default_settings/ldapauth.py
+++ apis_acdhch_default_settings/ldapauth.py
@@ -1,11 +1,21 @@
 import os
+import pathlib
+import logging
 
 from django_auth_ldap.backend import LDAPBackend
+
+logger = logging.getLogger(__name__)
 
 
 class CustomLDAPBackend(LDAPBackend):
     def authenticate_ldap_user(self, user, *args, **kwargs):
-        user_list = os.environ.get("AUTH_LDAP_USER_LIST", "").split(",")
+        env = os.environ.get("AUTH_LDAP_USER_LIST", "")
+        user_list = env.split(",")
+        if env.startswith("file://"):
+            try:
+                user_list = pathlib.Path.from_uri(env).read_text().splitlines()
+            except Exception as e:
+                logger.debug("Error reading user list from file: %s", e)
         user_list = list(map(str.strip, user_list))
         if user._username not in user_list:
             return None
